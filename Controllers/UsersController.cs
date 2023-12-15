04bc380e3b45fd7b3ce2add5ed9fb413c1422ee1
xiaoming <47764491+xiaoming709394@users.noreply.github.com>
2023-12-15 09:23:03 +0800

    xiaoming

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using net6_web_api.DB;
+
+namespace net6_web_api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<LuckySheetController> _logger;
+        private readonly SchoolContext _context;
+
+        
+    }
+}
---
+++ Entity/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace net6_web_api.Entity
+{
+    public class LoginModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
